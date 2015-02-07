8d575d2bf4c19fea5000478aca71769702602cda
Simon Walker <s.r.walker101@googlemail.com>
2015-02-07 22:09:13 +0000

    Set up template with placeholders

---
+++ pipelinerun/templates/run-script.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+# DATE: {{ date }}
+
+BIAS_ACTIONS=({{ bias }})
+DARK_ACTIONS=({{ dark }})
+FLAT_ACTIONS=({{ flat }})
+SCIENCE_ACTIONS=({{ science }})
+PIPELINE_SHA="{{ pipeline_sha }}"
+
+main() {
+    source /ngts/pipedev/ParanalOutput/running-the-pipeline/init.sh
+    setup_{{ planetname }}
+
+    RUN_NAME={{ date|clean_date }}-{{ planetname }}
+    ROOT_DIRECTORY=${BASEDIR}/${RUN_NAME}
+    INITIAL_WCS_SOLUTION=$(python ./solution_mapping/query_for_solution.py --mysql --camera-id {{ camera_id }} --action ${SCIENCE_ACTIONS[0]})
+
+    setup_actions_manually
+
+    python ${AUX_DIR}/build-pipeline-directory.py -o ${ROOT_DIRECTORY} $(cat ${ACTIONLIST})
+    cat $ACTIONLIST
+
+    upload_start_job_status
+
+    run_pipeline
+
+    report_job_status
+}
+
+main
