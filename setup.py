2c7e26be73ce418910e3674f5ae97ef3448f9e5b
Simon Walker <s.r.walker101@googlemail.com>
2015-02-07 22:01:10 +0000

    Start main test

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pipelinerun',
+    packages=find_packages(),
+)
---
+++ testing/test_overall_script.py
@@ -0,0 +1,25 @@
+from pipelinerun.cli import create_run_script
+
+class Arguments(object): pass
+
+def test_final_product(tmpdir):
+    with open('testing/examples/run-40-wasp18b-20141105.sh') as infile:
+        expected_contents = infile.read()
+
+
+    args = Arguments()
+    args.date = '2014/11/05'
+    args.bias = [101141,]
+    args.dark = [101143,]
+    args.flat = [101126,]
+    args.science = [101140,]
+    args.sha = '59aa1ec756657430048c45beea8093ed724f5ea2'
+    args.planetname = 'wasp18b'
+    args.camera_id = 804
+    args.output = str(tmpdir.join('out.sh'))
+
+    create_run_script(args)
+
+    with open(args.output) as infile:
+        assert infile.read() == expected_contents
+
