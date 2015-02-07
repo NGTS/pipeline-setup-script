b73da4f79566454bfb21476efdd55a25c378f9c5
Simon Walker <s.r.walker101@googlemail.com>
2015-02-07 22:26:44 +0000

    Add main run function

--- pipelinerun/cli.py
+++ pipelinerun/cli.py
@@ -1,3 +1,5 @@
+import argparse
+
 from .render import RendersTemplate
 
 
@@ -15,3 +17,19 @@ def create_run_script(args):
 
     with open(args.output, 'w') as outfile:
         outfile.write(text + '\n')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date', required=True)
+    parser.add_argument('-b', '--bias', nargs='+', required=True, type=int)
+    parser.add_argument('-d', '--dark', nargs='+', required=True, type=int)
+    parser.add_argument('-f', '--flat', nargs='+', required=True, type=int)
+    parser.add_argument('-s', '--science', nargs='+', required=True, type=int)
+    parser.add_argument('--sha', required=True)
+    parser.add_argument('--planet', required=True)
+    parser.add_argument('-c', '--camera_id', required=True, type=int)
+    create_run_script(parser.parse_args())
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='pipelinerun',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'render-pipelinescript.py = pipelinerun.cli:main',
+        ],
+    }
 )
