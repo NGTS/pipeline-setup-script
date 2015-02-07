6444f95b6f42e4a76e9433c2696775955ceb9798
Simon Walker <s.r.walker101@googlemail.com>
2015-02-07 22:14:45 +0000

    Add clean_date impementation

--- pipelinerun/templates.py
+++ pipelinerun/templates.py
@@ -1,9 +1,11 @@
 import jinja2
 
 
+def clean_date(datestr):
+    return datestr.replace('/', '')
+
 def create_env():
     env = jinja2.Environment(
         loader=jinja2.PackageLoader('pipelinerun', 'templates'))
-
-    env.filters['clean_date'] = lambda x: x
+    env.filters['clean_date'] = clean_date
     return env
--- testing/test_templates.py
+++ testing/test_templates.py
@@ -1,6 +1,11 @@
-from pipelinerun.templates import create_env
+from pipelinerun.templates import create_env, clean_date
 
 
 def test_template_loader():
     env = create_env()
     assert env.get_template('run-script.sh')
+
+
+def test_clean_date():
+    datestr = '2015/01/02'
+    assert clean_date(datestr) == '20150102'
