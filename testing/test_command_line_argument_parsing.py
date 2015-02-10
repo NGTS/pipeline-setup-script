d9ef87c78c1b06c09b1f9ddda9e87f4658c18330
Simon Walker <s.r.walker101@googlemail.com>
2015-02-10 10:45:09 +0000

    Add nice fixtures

--- testing/test_command_line_argument_parsing.py
+++ testing/test_command_line_argument_parsing.py
@@ -7,9 +7,25 @@ import pytest
 from pipelinerun import cli
 
 
+@pytest.fixture
+def valid_args():
+    return ['--date', '2015/01/26', '-b', '101266',
+            '-d', '101255', '-f', '101219', '101223', '-s', '101221',
+            '--planet', 'wasp18', '-c', '803']
+
+
+@pytest.fixture
+def parser():
+    return cli.create_parser()
+
+
+@pytest.fixture
+def args(parser, valid_args):
+    return parser.parse_args(valid_args)
+
+
 @mock.patch('pipelinerun.cli.argparse.ArgumentParser.exit')
-def test_parse_args_help(exit, capsys):
-    parser = cli.create_parser()
+def test_parse_args_help(exit, parser, capsys):
     parser.parse_args(['-h'])
 
     out, err = capsys.readouterr()
