3a70a9dca9df4c0c756502857ae4d6c52a42e9d0
Simon Walker <s.r.walker101@googlemail.com>
2015-02-10 09:17:37 +0000

    Add requirements to setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pipelinerun',
     packages=find_packages(),
+    install_requires=['jinja2', ],
     entry_points={
         'console_scripts': [
             'render-pipelinescript.py = pipelinerun.cli:main',
