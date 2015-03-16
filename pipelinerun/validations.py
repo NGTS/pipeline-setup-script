45477304735169a6f2eca4d9846a029bc415aa11
Simon Walker <s.r.walker101@googlemail.com>
2015-03-16 13:28:43 +0000

    Add check for valid number of files even if dir exists

--- pipelinerun/validations.py
+++ pipelinerun/validations.py
@@ -19,6 +19,13 @@ class NotFound(RuntimeError):
         super(NotFound, self).__init__(msg)
 
 
+class NoFiles(RuntimeError):
+
+    def __init__(self, path):
+        msg = 'Cannot find any files in {}'.format(path)
+        super(NoFiles, self).__init__(msg)
+
+
 def ensure_all_actions_available(bias, dark, flat, science):
     '''
     Given lists of actions, ensure the directories are all available on disk
@@ -27,14 +34,18 @@ def ensure_all_actions_available(bias, dark, flat, science):
     keys = ['bias', 'dark', 'flat', 'science']
     for (key, values) in zip(keys, [bias, dark, flat, science]):
         for action_id in values:
-            found = False
+            found, action_path = False, None
             dirname_options = [os.path.join(data_root,
                                             'action{action}_{suffix}'.format(
                                                 action=action_id, suffix=suffix))
                                for suffix in suffixes[key]]
             for option in dirname_options:
                 if os.path.isdir(option):
-                    found = True
+                    found, action_path = True, option
                     break
             if not found:
                 raise NotFound(action_id, dirname_options)
+            else:
+                files = os.listdir(action_path)
+                if not len(files) > 0:
+                    raise NoFiles(action_path)
