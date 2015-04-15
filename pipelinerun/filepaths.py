f04d4495ac71e234071129df2c4313a41db437a6
Simon Walker <s.r.walker101@googlemail.com>
2015-04-15 08:58:11 +0100

    Correct filepaths module to use night parameter

--- pipelinerun/filepaths.py
+++ pipelinerun/filepaths.py
@@ -4,9 +4,9 @@ ROOT_DIR = os.path.join('/', 'ngts', 'pipedev', 'ParanalOutput',
                         'running-the-pipeline', 'run-scripts')
 
 
-def default_filename(date, planet_name, camera_id):
-    file_stub = '{date}-{planet_name}-{camera_id}.sh'.format(
-        date=date,
+def default_filename(night, planet_name, camera_id):
+    file_stub = '{night}-{planet_name}-{camera_id}.sh'.format(
+        night=night,
         planet_name=planet_name,
         camera_id=camera_id)
     return os.path.join(ROOT_DIR, file_stub)
